95dfdb616ab3ce22de0e3d1860b7eebcf5929085
Reto Seiz <reto.seiz@gmail.com>
2018-12-09 07:24:40 +0100

    good for part 1, good enough for part 2?

--- AdventOfCode2018/Day09/Day09.cs
+++ AdventOfCode2018/Day09/Day09.cs
@@ -8,18 +8,88 @@ namespace AdventOfCode2018.Day09
 {
     internal class Day09 : DayBase
     {
-        public override string Title => "";
+        public override string Title => "--- Day 9: Marble Mania ---";
 
         public override bool Ignore => false;
 
         public override string Part1(List<string> input, bool isTestRun)
         {
-            return input.Count.ToString();
+            return "skipped";
+            string answer = "";
+            foreach (string s in input)
+            {
+                answer += CalculateWinnerScore(s, false);
+            }
+            return answer;
+        }
+
+        private string CalculateWinnerScore(string s, bool part2)
+        {
+            List<string> pieces = s.Split(' ').ToList();
+            int numberOfPlayers = int.Parse(pieces[0]);
+            List<int> playerScores = new List<int>();
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                playerScores.Add(0);
+            }
+            int highestMarbleValue = int.Parse(pieces[6]);
+            if (part2)
+            {
+                highestMarbleValue = highestMarbleValue * 100;
+            }
+            List<int> marbles = new List<int>();
+            int currentPositionInList = 0;
+            marbles.Add(0);
+
+
+            for (int i = 1; i <= highestMarbleValue; i++)
+            {
+                //einfügen zwischen 1 & 2 nach rechts (im Uhrzeigersinn) dies ist dann die aktive kugel
+                int targetIndex = (currentPositionInList + 2) % marbles.Count;//endposition berechnen indem man über die loop-grenze hinaus kann
+                if (i % 23 == 0)//falls die kugel aber ein vielfaches von 23 ist wird sie behalten (+score)
+                {
+                    playerScores[i % numberOfPlayers] += i;
+                    // & die kugel 7 nach links (gegenuhrzeigersinn) wird entfernt (+ score)
+                    if (currentPositionInList >= 7)
+                    {
+                        targetIndex = (currentPositionInList - 7) % marbles.Count;
+                    }
+                    else
+                    {
+                        targetIndex = marbles.Count + currentPositionInList - 7;
+                    }
+
+                    playerScores[i % numberOfPlayers] += marbles[targetIndex];
+                    marbles.RemoveAt(targetIndex);
+                    // -> die kugel nach rechts ist die neue aktive kugel im kreis
+                }
+                else
+                {
+                    marbles.Insert(targetIndex, i);
+                }
+                currentPositionInList = targetIndex;
+
+            }
+
+            return $"players {numberOfPlayers}, max-marble {highestMarbleValue}, winning-score {playerScores.Max()}       ";
+            //return playerScores.IndexOf(winnerScore).ToString();
+
+
+            //return input.Count.ToString();
         }
 
         public override string Part2(List<string> input, bool isTestRun)
         {
-            return input.Count.ToString();
+            if (isTestRun)
+            {
+                return "skipped";
+            }
+            string answer = "";
+            foreach (string s in input)
+            {
+                answer += CalculateWinnerScore(s, true);
+            }
+            return answer;
         }
     }
 }
