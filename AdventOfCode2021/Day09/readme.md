d38b3f8b07565488279c2d233803e5c9ecdba419
Reto Seiz <reto.seiz@roche.com>
2021-12-10 07:40:41 +0100

    day 10

--- AdventOfCode2021/Day09/readme.md
+++ AdventOfCode2021/Day09/readme.md
@@ -1,4 +1,4 @@
 ﻿### a visualisation has been made
 
 If 'DebugMode' gets set to true it will create lots of html files. Whith the help of 'wkhtmltoimage' I created jpgs. With the help of 'avidemux' I created a movie from the pictures.
-- There are a few samplefiles and scripts in the subfolder 'Pictures'
+- There are a few samplefiles and scripts in the subfolder 'Visualisation'
--- AdventOfCode2021/Day10/Day10.cs
+++ AdventOfCode2021/Day10/Day10.cs
@@ -14,24 +14,155 @@ internal class Day10 : DayBase
         //visualisation txt->jpg with irfanview, jpg frames->mp4 with avidemux Day 05 2021
         //visualisation png                                         Day 10 2018
         //dictionary                                                Day 06 2021
+        return "";
+        int sum = 0;
+
+        foreach (string line in input)
+        {
+            sum += CheckForChunks(line, 0);
+        }
 
 
-
-        return $"nothing";
+        return $"{sum}";
     }
+
 
     public override string Part2(List<string> input, bool isTestRun)
     {
-        //X,Y map  (+modulo)                                        Day 03 2020 https://adventofcode.com/2020/day/3
-        //map[,]   (+output to console, write to textfile)          Day 05 2021
-        //switch case string & int parsing                          Day 02 2021
-        //convert binaryint to decimal  Convert.ToInt32(string, 2)  Day 03 2021
-        //visualisation txt->jpg with irfanview, jpg frames->mp4 with avidemux Day 05 2021
-        //visualisation png                                         Day 10 2018
-        //dictionary                                                Day 06 2021
+        List<ulong> scores = new List<ulong>();
 
+        foreach (string line in input)
+        {
+            ulong score = CheckForChunksPart2(line, 0);
+            if (score>0)
+            {
+                scores.Add(score);
+            }
+            
+        }
 
+        scores.Sort();
 
-        return $"nothing";
+        return $"{scores[scores.Count() / 2]}";
+    }
+    private int CheckForChunks(string line, int startPos)
+    {
+        Stack<char> stack = new Stack<char>();
+        for (int i = startPos; i < line.Length; i++)
+        {
+            if (stack.Count() > 0 && stack.Peek() == line[i])
+            {
+                stack.Pop();
+            }
+            else if (new char[] {'[', '{', '(', '<' }.Contains(line[i]))
+            {
+                
+                switch (line[i])
+                {
+                    case '(':
+                        stack.Push(')');
+                        break;
+                    case '[':
+                        stack.Push(']');
+                        break;
+                    case '{':
+                        stack.Push('}');
+                        break;
+                    case '<':
+                        stack.Push('>');
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                //error
+                switch (line[i])
+                {
+                    case ')':
+                        return 3;
+                        break;
+                    case ']':
+                        return 57;
+                        break;
+                    case '}':
+                        return 1197;
+                        break;
+                    case '>':
+                        return 25137;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+
+        return 0;//all good
+    }
+    private ulong CheckForChunksPart2(string line, int startPos)
+    {
+        Stack<char> stack = new Stack<char>();
+        for (int i = startPos; i < line.Length; i++)
+        {
+            if (stack.Count() > 0 && stack.Peek() == line[i])
+            {
+                stack.Pop();
+            }
+            else if (new char[] { '[', '{', '(', '<' }.Contains(line[i]))
+            {
+
+                switch (line[i])
+                {
+                    case '(':
+                        stack.Push(')');
+                        break;
+                    case '[':
+                        stack.Push(']');
+                        break;
+                    case '{':
+                        stack.Push('}');
+                        break;
+                    case '<':
+                        stack.Push('>');
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                //error, ignore
+                return 0;
+            }
+
+        }
+        if (stack.Count>0)
+        {
+            ulong points = 0;
+            foreach (char c in stack)
+            {
+                switch (c)
+                {
+                    case ')':
+                        points = (points * 5) + 1;
+                        break;
+                    case ']':
+                        points = (points * 5) + 2;
+                        break;
+                    case '}':
+                        points = (points * 5) + 3;
+                        break;
+                    case '>':
+                        points = (points * 5) + 4;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return points;
+        }
+        return 0;//all good, ignore
     }
 }
