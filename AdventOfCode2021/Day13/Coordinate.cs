bfaa18783b2b3e61a5e03101ab5e62d2a42d9543
Reto Seiz <reto.seiz@roche.com>
2021-12-13 08:46:31 +0100

    Day 13

---
+++ AdventOfCode2021/Day13/Coordinate.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2021.Day13
+{
+    internal class Coordinate
+    {
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int Y { get; internal set; }
+        public int X { get; internal set; }
+    }
+}
---
+++ AdventOfCode2021/Day13/Day13.cs
@@ -0,0 +1,190 @@
+﻿using System.Drawing;
+
+namespace AdventOfCode2021.Day13;
+
+internal class Day13 : DayBase
+{
+    public override string Title => "--- Day 13 ---";
+    public override bool Ignore => false;
+    public List<Coordinate> Coordinates { get; set; }
+    public List<Tuple<char, int>> FoldInstructions { get; set; }
+    public int PaperHeight { get; set; }
+    public int PaperWidth { get; set; }
+
+    public override string Part1(List<string> input, bool isTestRun)
+    {
+        ReaderInput(input);
+        PaperHeight = Coordinates.Max(c => c.Y);//start at 0
+        PaperWidth = Coordinates.Max(c => c.X);
+        //if (isTestRun) PrintPaper();
+
+        int nextHeight;
+        int nextWidth;
+
+        foreach (Tuple<char, int> fold in FoldInstructions)
+        {
+            if (fold.Item1 == 'y')
+            {
+                int upperHeight = PaperHeight - fold.Item2;
+                int lowerHeight = PaperHeight - upperHeight;
+                nextHeight = (upperHeight > lowerHeight) ? upperHeight : lowerHeight;
+                nextHeight--;
+                nextWidth = PaperWidth;
+
+                //fold up
+                foreach (Coordinate coord in Coordinates)
+                {
+                    if (coord.Y > fold.Item2)//below fold
+                    {
+                        coord.Y = PaperHeight - coord.Y + upperHeight % lowerHeight;//add what the upper is 'longer' in order to shift down                        
+                    }
+                }
+            }
+            else //x
+            {
+                //fold left
+                int leftWith = PaperWidth - fold.Item2;
+                int rightWidht = PaperWidth - leftWith;
+                nextHeight = PaperHeight;
+                nextWidth = (leftWith > rightWidht)? leftWith : rightWidht ;
+                nextWidth--;
+
+                //fold left
+                foreach (Coordinate coord in Coordinates)
+                {
+                    if (coord.X > fold.Item2)//right of the fold
+                    {
+                        coord.X = PaperWidth - coord.X + rightWidht % leftWith;//add what the left is 'longer' in order to shift left                        
+                    }
+                }
+            }
+
+            PaperHeight = nextHeight;
+            PaperWidth = nextWidth;
+
+            //if (isTestRun) PrintPaper();
+            //remove duplicates
+            Coordinates = Coordinates.GroupBy(c => $"{c.X}-{c.Y}").Select(x => x.First()).ToList();
+
+            return $"{Coordinates.Count()}";
+        }
+
+
+        return $"todoq";
+    }
+
+    private void PrintPaper()
+    {
+        Console.Clear(); ;
+        for (int x = 0; x <= PaperHeight; x++)
+        {
+            for (int y = 0; y <= PaperWidth; y++)
+            {
+                Console.SetCursorPosition(y, x);
+                Console.Write('.');
+            }
+        }
+        foreach (Coordinate coordinate in Coordinates)
+        {
+            Console.SetCursorPosition(coordinate.X, coordinate.Y);
+            Console.Write('#');
+        }
+    }
+
+    private void ReaderInput(List<string> input)
+    {
+        bool gapFound = false;
+        List<Coordinate> coordinates = new();
+        FoldInstructions = new();
+        foreach (string line in input)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                gapFound = true;
+            }
+            else if (gapFound)
+            {
+                string[] parts = line.Split(' ');
+                char axis = parts[2].Split('=')[0][0];
+                int foldOn = int.Parse(line.Split('=')[1]);
+                FoldInstructions.Add(Tuple.Create(axis, foldOn));
+            }
+            else
+            {
+                string[] parts = line.Split(',');
+                int x = int.Parse(parts[0]);
+                int y = int.Parse(parts[1]);
+                coordinates.Add(new Coordinate(x, y));    
+            }
+        }
+        Coordinates = coordinates;
+    }
+
+    public override string Part2(List<string> input, bool isTestRun)
+    {
+        ReaderInput(input);
+        PaperHeight = Coordinates.Max(c => c.Y);//start at 0
+        PaperWidth = Coordinates.Max(c => c.X);
+        //if (isTestRun) PrintPaper();
+
+        int nextHeight;
+        int nextWidth;
+
+        foreach (Tuple<char, int> fold in FoldInstructions)
+        {
+            if (fold.Item1 == 'y')
+            {
+                int upperHeight = PaperHeight - fold.Item2;
+                int lowerHeight = PaperHeight - upperHeight;
+                nextHeight = (upperHeight > lowerHeight) ? upperHeight : lowerHeight;
+                nextHeight--;
+                nextWidth = PaperWidth;
+
+                //fold up
+                foreach (Coordinate coord in Coordinates)
+                {
+                    if (coord.Y > fold.Item2)//below fold
+                    {
+                        int newY = PaperHeight - coord.Y + lowerHeight % upperHeight;//add what the upper is 'longer' in order to shift down         
+                        coord.Y = newY;               
+                    }
+                }
+            }
+            else //x
+            {
+                //fold left
+                int leftWith = PaperWidth - fold.Item2;
+                int rightWidht = PaperWidth - leftWith;
+                nextHeight = PaperHeight;
+                nextWidth = (leftWith > rightWidht) ? leftWith : rightWidht;
+                nextWidth--;
+
+                //fold left
+                foreach (Coordinate coord in Coordinates)
+                {
+                    if (coord.X > fold.Item2)//right of the fold
+                    {
+                        int newX = PaperWidth - coord.X + rightWidht % leftWith;//add what the left is 'longer' in order to shift left                         
+                        coord.X = newX;
+                    }
+                }
+            }
+
+            PaperHeight = nextHeight;
+            PaperWidth = nextWidth;
+
+            //if (isTestRun) PrintPaper();
+            //remove duplicates
+            Coordinates = Coordinates.GroupBy(c => $"{c.X}-{c.Y}").Select(x => x.First()).ToList();
+        }
+
+        Bitmap bitmap = new Bitmap(PaperWidth + 1, PaperHeight+ 1);
+        foreach (Coordinate c in Coordinates)
+        {
+            bitmap.SetPixel(c.X, c.Y, Color.Black);
+        }
+        bitmap.Save($"AoC_2021_Day13_IsTestRun-{isTestRun}.png");
+
+        return $"See generated picture in output folder";
+    }
+}
