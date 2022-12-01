c530d105378d83543edbc47c417475be9545b678
Reto Seiz <reto.seiz@gmail.com>
2022-12-01 20:19:23 +0100

    Day 1 2022

---
+++ AdventOfCode2022/Day01/Day01.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode2022.Day01;
+
+internal class Day01 : DayBase
+{
+    public override string Title => "--- Day 1: Calorie Counting ---";
+    public override bool Ignore => false;
+
+    public override string Part1(List<string> input, bool isTestRun)
+    {
+        List<Elv> elves = new List<Elv>();
+        Elv elv = new Elv();
+        elves.Add(elv);
+        foreach (string line in input)
+        {
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                elv.Load.Add(long.Parse(line));
+            }
+            else
+            {
+                elv = new();
+                elves.Add(elv);
+            }
+        }
+
+        return elves.OrderByDescending(e => e.TotalLoad).First().TotalLoad.ToString();
+    }
+
+    public override string Part2(List<string> input, bool isTestRun)
+    {
+        List<Elv> elves = new List<Elv>();
+        Elv elv = new Elv();
+        elves.Add(elv);
+        foreach (string line in input)
+        {
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                elv.Load.Add(long.Parse(line));
+            }
+            else
+            {
+                elv = new();
+                elves.Add(elv);
+            }
+        }
+
+        return elves.OrderByDescending(e => e.TotalLoad).Take(3).Select(e => e.TotalLoad).Sum().ToString();
+    }
+}
+
---
+++ AdventOfCode2022/Day01/Elv.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day01
+{
+    public class Elv
+    {
+        public List<long> Load { get; set; } = new List<long>();
+        public long TotalLoad { get { return Load.Sum(); } }
+    }
+}
