0abc89de0b713fc7d271f1a8ad0b989d5018b7c8
Reto Seiz <reto.seiz@gmail.com>
2022-12-04 06:23:56 +0100

    Day 4

---
+++ AdventOfCode2022/Day04/Day04.cs
@@ -0,0 +1,34 @@
+﻿namespace AdventOfCode2022.Day04;
+
+internal class Day04 : DayBase
+{
+    public override string Title => "--- Day 4: Camp Cleanup ---";
+    public override bool Ignore => true;
+
+    public override string Part1(List<string> input, bool isTestRun)
+    {
+        List<Elv> elves= new List<Elv>();
+        foreach (string line in input)
+        {
+            elves.Add(new Elv(line));
+        }
+
+
+
+        return elves.Count(e => e.OneContainsTheOther).ToString();
+    }
+
+    public override string Part2(List<string> input, bool isTestRun)
+    {
+        List<Elv> elves = new List<Elv>();
+        foreach (string line in input)
+        {
+            elves.Add(new Elv(line));
+        }
+
+
+
+        return elves.Count(e => e.AnyOverlap).ToString();
+    }
+}
+
---
+++ AdventOfCode2022/Day04/Elv.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day04
+{
+    public class Elv
+    {
+        string input = string.Empty;
+        long startA;
+        long endA;
+        long startB;
+        long endB;
+        public Elv(string input)
+        {
+            this.input = input;
+            long[] parts = input.Split(',')
+                .SelectMany(p => p.Split('-'))
+                .Select(x => long.Parse(x))
+                .ToArray();
+
+            startA = parts[0];
+            endA = parts[1];
+            startB = parts[2];
+            endB = parts[3];
+        }
+
+        public bool AnyOverlap
+        {
+            get
+            {
+                if (OneContainsTheOther)
+                {
+                    return true;
+                }
+                if (startA <= startB && endA >= startB)
+                {
+                    return true;
+                }
+                else if (startA <= endB && endA >= endB)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+        public bool OneContainsTheOther 
+        { 
+            get 
+            {
+                if (startA <= startB && endA >= endB)
+                {
+                    return true;
+                }
+                else if(startA >= startB && endA <= endB)
+                {
+                    return true;
+                }
+                return false;
+            }  
+        }
+    }
+}
