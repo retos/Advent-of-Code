f015af85435e6b2eef225d60e1a26a54bcb7f907
Reto Seiz <reto.seiz@gmail.com>
2022-12-08 18:45:22 +0100

    Day 8 (some cleanup might be beeded)

---
+++ AdventOfCode2022/Day08/Day08.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode2022.Day08;
+
+internal class Day08 : DayBase
+{
+    public override string Title => "--- Day 8: Treetop Tree House ---";
+    public override bool Ignore => false;
+    public Tree[,] Map { get; set; }
+
+    static Direction Left = new Direction(0, -1);
+    static Direction Right = new Direction(0, 1);
+    static Direction Up = new Direction(-1, 0);
+    static Direction Down = new Direction(1, 0);
+    record Direction(int drow, int dcol);
+    record TreeNode(int height, int irow, int icol);
+
+    public override string Part1(List<string> input, bool isTestRun)
+    {
+        GetMap(input);
+        return Map[0,0].AllNodes.Where(t => t.IsVisible()).Count().ToString();
+    }
+
+    public override string Part2(List<string> input, bool isTestRun)
+    {
+
+        GetMap(input);
+        Tree highScore = Map[0,0].AllNodes.OrderByDescending(n => n.GetScenicScore()).First();
+        //if (isTestRun)
+        //{
+        //    PrintScenicScoreToConsole();
+        //}
+        return highScore.GetScenicScore().ToString();        
+    }
+
+
+
+    private void PrintScenicScoreToConsole()
+    {
+        //Console.SetCursorPosition(Console.GetCursorPosition(), 0);
+        Console.WriteLine();
+        for (int y = 0; y < Map[0, 0].ZeileCount; y++)
+        {
+            for (int x = 0; x < Map[0, 0].SpaltenCount; x++)
+            {
+                Console.Write(Map[x, y].GetScenicScore());
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+    protected Tree[,] GetMap(List<string> input)
+    {
+        int xMapCount = input[0].Length;
+        int yMapCount = input.Count;
+
+        Map = new Tree[input[0].Length, input.Count];
+        List<Tree> allNodes = new List<Tree>();
+        for (int y = 0; y < input.Count; y++)
+        {
+            for (int x = 0; x < input[0].Length; x++)
+            {
+                int height = int.Parse(input[y][x].ToString());
+                Tree tree = new Tree(Map, x, y, xMapCount, yMapCount, allNodes);
+                tree.Height = height;
+                Map[x, y] = tree;
+            }
+        }
+        return Map;
+    }
+}
+
---
+++ AdventOfCode2022/Day08/Tree.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq;
+
+namespace AdventOfCode2022.Day08
+{
+    public class Tree
+    {
+        /// <summary>
+        /// Postion Spalte
+        /// </summary>
+        private int spalte;
+        /// <summary>
+        /// Position Zeile
+        /// </summary>
+        private int zeile;
+        /// <summary>
+        /// Sammllung aller Nodes als 2d Array
+        /// </summary>
+        private Tree[,] map;
+        /// <summary>
+        /// Sammlung aller Nodes als Liste
+        /// </summary>
+        private List<Tree> allNodes;
+        private int zeileCount;
+        private int spalteCount;
+        private bool? isVisible = null;
+
+        static Direction Left = new Direction(0, -1);
+        static Direction Right = new Direction(0, 1);
+        static Direction Up = new Direction(-1, 0);
+        static Direction Down = new Direction(1, 0);
+
+        public record Direction(int drow, int dcol);
+
+        IEnumerable<Tree> SmallerTrees(Direction dir) =>
+            TreesInDirection(dir).TakeWhile(treeT => treeT.Height < this.Height);
+
+        IEnumerable<Tree> TreesInDirection(Direction dir)
+        {
+            var (first, irow, icol) = (true, zeile, spalte);
+            while (irow >= 0 && irow < ZeileCount && icol >= 0 && icol < SpaltenCount)
+            {
+                if (!first)
+                {
+                    yield return map[icol, irow];
+                }
+                (first, irow, icol) = (false, irow + dir.drow, icol + dir.dcol);
+            }
+        }
+
+        public Tree(Tree[,] map, int x, int y, int xMapCount, int yMapCount, List<Tree> allNodes)
+        {
+            this.map = map;
+            this.spalte = x;
+            this.zeile = y;
+            this.SpaltenCount = xMapCount;
+            this.ZeileCount = yMapCount;
+            this.AllNodes = allNodes;
+            allNodes.Add(this);
+        }
+
+        public int Height { get; internal set; }
+        public bool IsVisible() =>
+            (IsTallest(Left) || IsTallest(Right) || IsTallest(Up) || IsTallest(Down));
+
+        public bool IsTallest(Direction dir) =>
+            TreesInDirection(dir).All(treeT => treeT.Height < this.Height);
+
+        public long ViewDistance(Direction dir) =>
+            IsTallest(dir) ? TreesInDirection(dir).Count() : SmallerTrees(dir).Count() + 1;
+
+        public List<Tree> AllNodes { get => allNodes; set => allNodes = value; }
+        public int ZeileCount { get => zeileCount; set => zeileCount = value; }
+        public int SpaltenCount { get => spalteCount; set => spalteCount = value; }
+
+        private bool IsEdgeTree()
+        {
+            return spalte == 0 || zeile == 0 || spalte == SpaltenCount - 1 || zeile == ZeileCount - 1;
+        }
+
+        internal long GetScenicScore()
+        {
+            if (IsEdgeTree()) { return 0; };
+            return ViewDistance(Up) * ViewDistance(Down) * ViewDistance(Left) * ViewDistance(Right);
+        }
+    }
+}
